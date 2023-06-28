36921d9e6e1f7f938d4ccee31edcac42440cbbac
Adrian-Searles <121983439+Adrian-Searles@users.noreply.github.com>
2023-06-28 16:29:58 -0400

    Add Inventory and weapon system(database incoming)

---
+++ weapons.py
@@ -0,0 +1,37 @@
+
+
+#Define classes for each weapon with predefined attributes. 
+#Define more as needed
+class Weapon:
+    def __init__(self, name, damage, durability):
+        self.name = name
+        self.damage = damage
+        self.durability = durability
+
+    def createweapon(object,damage,durability):
+       return object("scalpel", damage, durability)
+       
+
+
+
+class Sword(Weapon):
+    def __init__(self, name, damage, durability):
+        super().__init__(name, damage, durability)
+
+class Bow(Weapon):
+    def __init__(self, name, damage, durability):
+        super().__init__(name, damage, durability)
+
+class Scalpel(Weapon):
+    def __init__(self, name, damage, durability):
+        super().__init__(name, damage, durability)
+
+
+class Gun(Weapon):
+    def __init__(self, name, damage, durability):
+        super().__init__(name, damage, durability)
+
+
+class MorningStar(Weapon):
+    def __init__(self, name, damage, durability):
+        super().__init__(name, damage, durability)
